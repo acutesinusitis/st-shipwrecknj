53dd65d4abff00dcdf72e7d21f6d912aa6d50a9f
acutesinusitis <132495217+acutesinusitis@users.noreply.github.com>
2023-05-03 19:35:41 -0400

    ship-app.py

--- ship-app.py
+++ ship-app.py
@@ -1,10 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import streamlit as st
-from PIL import Image
-import plotly.express as px #New Package
-import pydeck as pdk
-
 image1 = "C:/Users/acute/OneDrive - Bentley University/shipwrecks_1.jpg"
 
 path = "C:/Users/acute/OneDrive - Bentley University/"
@@ -18,48 +11,26 @@ def page1():
     st.image(image)
     shipwreck_count = len(df)
     st.header(f"At this time there are currently {shipwreck_count} shipwrecks along the coast of New Jersey with identified coordinates.\n")
-    header2 = '<p style="font-family:Serif; color:Blue; font-size: 25px;">Below is a list of all the names of the ships in the map above with the given coordinates:'
-    st.markdown(header2, unsafe_allow_html=True)
+    header1 = '<p style="font-family:Serif; color:#00CED1; font-size: 25px;">Below is a list of all the names of the ships in the map above with the given coordinates:'
+    st.markdown(header1, unsafe_allow_html=True)
     df_page_1 = df.loc[:, ["SHIP'S NAME", "LOCATION LOST", "LATITUDE", "LONGITUDE"]]
     st.write(df_page_1)
-    header1 = '<p style="font-family:Serif; color:Blue; font-size: 25px;">Below is a map of all the coordinates to sunken ships:'
-    st.markdown(header1, unsafe_allow_html=True)
+    header2 = '<p style="font-family:Serif; color:#00CED1; font-size: 25px;">Below is a map of all the coordinates to sunken ships:'
+    st.markdown(header2, unsafe_allow_html=True)
     df2 = df_page_1.drop(columns=["LOCATION LOST"])
     df2.rename(columns={"LATITUDE": "lat", "LONGITUDE": "lon"}, inplace=True)
     ICON_URL = "https://upload.wikimedia.org/wikipedia/commons/2/23/Map_marker_icon_%E2%80%93_Nicolas_Mollet_%E2%80%93_Shipwreck_%E2%80%93_Industry_%E2%80%93_Light.png"
-    icon_data = {
-        "url": ICON_URL,
-        "width": 100,
-        "height": 100,
-        "anchorY": 1
-    }
+    icon_data = {"url": ICON_URL,"width": 100,"height": 100,"anchorY": 1}
     df2["icon_data"] = None
     for i in df2.index:
         df2["icon_data"][i] = icon_data
-    icon_layer = pdk.Layer(type="IconLayer",
-                           data=df2,
-                           get_icon="icon_data",
-                           get_position='[lon,lat]',
-                           get_size=2,
-                           size_scale=10,
-                           pickable=True)
-    view_state = pdk.ViewState(
-        latitude=df2["lat"].mean(),
-        longitude=df2["lon"].mean(),
-        zoom=6,
-        pitch=0
-    )
-    tool_tip = {"html": "Ship Name:<br/> <b>{SHIP'S NAME}</b>",
-                "style": {"backgroundColor": "blue",
-                          "color": "white"}
-                }
-
-    icon_map = pdk.Deck(
-        map_style='mapbox://styles/mapbox/satellite-v9',
-        layers=[icon_layer],
-        initial_view_state=view_state,
-        tooltip=tool_tip)
+    icon_layer = pdk.Layer(type="IconLayer",data=df2,get_icon="icon_data",get_position='[lon,lat]',get_size=2,size_scale=10,pickable=True)
+    view_state = pdk.ViewState(latitude=df2["lat"].mean(),longitude=df2["lon"].mean(),zoom=6,pitch=0)
+    tool_tip = {"html": "Ship Name:<br/> <b>{SHIP'S NAME}</br> <b>{lat}, {lon}</b>", "style": {"backgroundColor": "blue", "color": "white"}}
+    icon_map = pdk.Deck(map_style='mapbox://styles/mapbox/satellite-v9',layers=[icon_layer],initial_view_state=view_state,tooltip=tool_tip)
     st.pydeck_chart(icon_map)
+    header3 = '<p style="font-family:Serif; color:#00CED1; font-size: 25px;">More details regarding the minimum and maximum coordinates:'
+    st.markdown(header3, unsafe_allow_html=True)
 
 def page2():
     st.title("Ship Analytics")
@@ -79,13 +50,15 @@ def page2():
     plt.xlabel("Year Ranges")
     plt.ylabel("Frequency")
     st.pyplot() #Bar Chart
-    st.header("As we can see from the graph above, the year range with the highest amount of shipwrecks was between 1850-1900.") #hardcode?
+    most_frequent_range = df2["year_range"].value_counts().idxmax()
+    st.header(f"As we can see from the graph above, the year range with the highest amount of shipwrecks was between {most_frequent_range}.")
     df3 = df.loc[:, ["CONSTRUCTION"]].sort_values("CONSTRUCTION", ascending=True)
     construction_counts = df3["CONSTRUCTION"].value_counts()
     fig = px.pie(values=construction_counts.values, names=construction_counts.index, title= "Materials Used for Ship Construction")#New Package
     fig.update_layout(legend_title="Materials") #New Package/Pie Chart
     st.plotly_chart(fig, use_container_width=True) #New Package/Pie Chart
-    st.header("As we can see from the graph above, wood was by far the most frequent material used in constructing the sunken ships.") #hardcode?/HELP can use multiple pies?
+    most_frequent_range2 = df3["CONSTRUCTION"].value_counts().idxmax()
+    st.header(f"As we can see from the graph above, {most_frequent_range2.lower()} was by far the most frequent material used in constructing the sunken ships.")
     df4 = df.loc[:, ["VESSEL TYPE"]].sort_values("VESSEL TYPE", ascending=True)
     vessel_counts = df4["VESSEL TYPE"].value_counts()
     OTHER_LIMIT = 5
@@ -94,11 +67,11 @@ def page2():
     vessel_counts = df4["VESSEL TYPE"].value_counts()
     fig2 = px.pie(values=vessel_counts.values, names=vessel_counts.index,
                  title="Types of Vessels", hole=.3)  # New Package
-    fig2.update_layout(legend_title="Vessel Types")  # New Package/Pie Chart
-    st.plotly_chart(fig2, use_container_width=True)  # New Package/Pie Chart
+    fig2.update_layout(legend_title="Vessel Types")  # New Package/Donut Chart
+    st.plotly_chart(fig2, use_container_width=True)  # New Package/Donut Chart
     st.write(f"Note: All ship types with less than {OTHER_LIMIT} data points are grouped into the other category.")
-    st.header(
-        "As we can see from the graph above, Schooners were by far the most frequent material of the sunken ships.")  # hardcode?/HELP can use multiple pies?
+    most_frequent_range3 = df4["VESSEL TYPE"].value_counts().idxmax()
+    st.header(f"As we can see from the graph above, {most_frequent_range3}s were by far the most frequent material of the sunken ships.")
 
 def page3():
     st.title("Value of Treasure")
@@ -118,27 +91,42 @@ def page3():
     & (df_page_2["CARGO VALUE"] >= min_values2) & (df_page_2["CARGO VALUE"] <= max_values2)][["SHIP'S NAME", "SHIP VALUE", "CARGO VALUE"]]
     df_page_2 = df_page_2.replace(0, "No Data")
     st.write(df_page_2)
-    return st.write(f"All values are dollar denominated.")
+    def min_nonzero(x):
+        nonzero_vals = x[x != 0]
+        if len(nonzero_vals) > 0:
+            return np.min(nonzero_vals)
+        else:
+            return 0
+    top_3_columns = df["CONSTRUCTION"].value_counts().nlargest(3).index.tolist()
+    pivot_table = pd.pivot_table(df[df["CONSTRUCTION"].isin(top_3_columns)], values="SHIP VALUE", index="CONSTRUCTION", aggfunc=[min_nonzero,max]) # Pivot Tables
+    pivot_table2 = pd.pivot_table(df[df["CONSTRUCTION"].isin(top_3_columns)].iloc[:], values="CARGO VALUE", index="CONSTRUCTION",
+                                 aggfunc=[min_nonzero, max])
+    st.header("The minimum and maximum Ship Values from the three most common construction materials.")
+    st.write(pivot_table)
+    st.header("The minimum and maximum Cargo Values from the three most common construction materials.")
+    st.write(pivot_table2)
+    st.write(f"All values are dollar denominated.")
 
 
 def page4():
     st.title("Ship Dimension Specifications")
-    min_value = 0 #CAPITALIZE
-    max_value_length = 700
-    max_value_beam = 100
-    max_value_draft = 105
-    max_value_gross = 29100
-    default_value_length = [0, 700]
-    default_value_beam = [0, 100]
-    default_value_draft = [0, 105]
-    default_value_gross = [0, 29100]
-    min_values, max_values= st.slider("Select a ship length range:", min_value, max_value_length, default_value_length, 5)
+    MIN_VALUE = 0
+    INCREMENT = 5
+    MAX_VALUE_LENGTH = 700
+    MAX_VALUE_BEAM = 100
+    MAX_VALUE_DRAFT = 105
+    MAX_VALUE_GROSS = 29100
+    DEFAULT_VALUE_LENGTH = [0, 700]
+    DEFAULT_VALUE_BEAM = [0, 100]
+    DEFAULT_VALUE_DRAFT = [0, 105]
+    DEFAULT_VALUE_GROSS = [0, 29100]
+    min_values, max_values= st.slider("Select a ship length range:", MIN_VALUE, MAX_VALUE_LENGTH, DEFAULT_VALUE_LENGTH, INCREMENT)
     st.write(f"Selected ship length range: {min_values} to {max_values}")
-    min_values2, max_values2 = st.slider("Select a ship beam range:", min_value, max_value_beam, default_value_beam, 5)
+    min_values2, max_values2 = st.slider("Select a ship beam range:", MIN_VALUE, MAX_VALUE_BEAM, DEFAULT_VALUE_BEAM, INCREMENT)
     st.write(f"Selected ship beam range: {min_values2} to {max_values2}")
-    min_values3, max_values3 = st.slider("Select a ship draft range:", min_value, max_value_draft, default_value_draft, 5)
+    min_values3, max_values3 = st.slider("Select a ship draft range:", MIN_VALUE, MAX_VALUE_DRAFT, DEFAULT_VALUE_DRAFT, INCREMENT)
     st.write(f"Selected ship draft range: {min_values3} to {max_values3}")
-    min_values4, max_values4 = st.slider("Select a ship gross tonnage range:", min_value, max_value_gross, default_value_gross, 5)
+    min_values4, max_values4 = st.slider("Select a ship gross tonnage range:", MIN_VALUE, MAX_VALUE_GROSS, DEFAULT_VALUE_GROSS, INCREMENT)
     st.write(f"Selected ship gross tonnage range: {min_values4} to {max_values4}")
     df["LENGTH"] = pd.to_numeric(df["LENGTH"])
     df["BEAM"] = pd.to_numeric(df["BEAM"])
@@ -153,27 +141,33 @@ def page4():
     df_page_3 = df_page_3.replace(0, "No Data")
     st.write(df_page_3)
     st.write("Length, Beam, and Draft Measurements are in Feet. Gross Tonnage is in Tons.")
-    columns = ["LENGTH", "BEAM", "DRAFT", "GROSS TONNAGE"]
-    column_name = st.selectbox("Please select a column: ", columns)
-    st.write("You have selected", column_name)
+
+def main():
+    menu = ["Map", "Ship Analytics", "Value of Treasure", "Ship Dimension Specifications"]
+    choice = st.sidebar.selectbox("Select an Option:", menu)
     def minmax(column_name, df = "C:/Users/acute/OneDrive - Bentley University/Working Shipwreck Database.csv"):
         non_zero = df[df[column_name] != 0][column_name]  # Exclude zero values
         min_val = non_zero.min()
         max_val = df[column_name].max()
         return min_val, max_val
-    st.write("The minimum measurement is", minmax(column_name, df)[0], "and the maximum measurement is", minmax(column_name, df)[-1]) #HELP f-string
-
-
-def main():
-    menu = ["Map", "Ship Analytics", "Value of Treasure", "Ship Dimension Specifications"]
-    choice = st.sidebar.selectbox("Select an Option:", menu)
     if choice == "Map":
         page1()
+        columns = ["LATITUDE", "LONGITUDE"]
+        column_name = st.selectbox("Please select a column: ", columns)
+        st.write("You have selected", column_name)
+        string_min, string_max = str(minmax(column_name, df)[0]), str(minmax(column_name, df)[-1])
+        st.write(f"The minimum {column_name.lower()} is", string_min, f"and the maximum {column_name.lower()} is", string_max + ".")
     elif choice == "Ship Analytics":
         page2()
     elif choice == "Value of Treasure":
         page3()
     elif choice == "Ship Dimension Specifications":
         page4()
+        columns = ["LENGTH", "BEAM", "DRAFT", "GROSS TONNAGE"]
+        column_name = st.selectbox("Please select a column: ", columns)
+        st.write("You have selected", column_name)
+        string_min, string_max = str(minmax(column_name, df)[0]), str(minmax(column_name, df)[-1])
+        st.write("The minimum measurement is", string_min, "and the maximum measurement is",
+                 string_max + ".")
 if __name__ == '__main__':
     main()
